69ab905c06fa78e58a0f4c6062044ea439d83d22
gitkrishraj <krishraj30032005@gmail.com>
2025-03-12 00:28:15 +0530

    changes

--- src/Example.java
+++ src/Example.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Example {
     public static void main(String[] args) {
-        int[] arr = {3,5,2,1,4,7,3,6};
+        int[] arr = {3,5,2,1,4,7,6};
         Cyclic(arr);
         System.out.println(Arrays.toString(arr));
     }
