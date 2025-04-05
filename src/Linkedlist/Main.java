bc34edbd4fe64c1cf916f7078739b68fbef71ae1
gitkrishraj <krishraj30032005@gmail.com>
2025-04-06 01:21:44 +0530

    changes

---
+++ src/Linkedlist/Main.java
@@ -0,0 +1,20 @@
+package Linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+       LL list= new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(66);
+        list.insert(100,3);
+        list.display();
+        System.out.println(list.deleteFirst());
+        list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(2));
+        list.display();
+    }
+}
