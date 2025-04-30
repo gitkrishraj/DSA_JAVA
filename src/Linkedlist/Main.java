5c32857870fa52a59040918dbebd1dd580640624
gitkrishraj <krishraj30032005@gmail.com>
2025-05-01 02:17:44 +0530

    chnages

--- src/Linkedlist/Main.java
+++ src/Linkedlist/Main.java
@@ -2,20 +2,23 @@ package Linkedlist;
 
 public class Main {
     public static void main(String[] args) {
-//       LL list= new LL();
-//        list.insertFirst(3);
-//        list.insertFirst(2);
-//        list.insertFirst(8);
-//        list.insertFirst(17);
-//        list.insertLast(66);
-//        list.insert(100,3);
-//        list.display();
-//        System.out.println(list.deleteFirst());
-//        list.display();
-//        System.out.println(list.deleteLast());
-//        list.display();
-//        System.out.println(list.delete(2));
-//        list.display();
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(66);
+        list.insert(100, 3);
+        list.display();
+        System.out.println(list.deleteFirst());
+        list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(2));
+        list.display();
+
+    }
+}
 
 
 //     DLL list= new DLL();
@@ -27,13 +30,13 @@ public class Main {
 //     list.insert(2,45);
 //     list.diplay();
 
-        CLL list = new CLL();
-        list.insert(33);
-        list.insert(23);
-        list.insert(13);
-        list.insert(76);
-        list.display();
-        list.delete(23);
-        list.display();
-    }
-}
+//        CLL list = new CLL();
+//        list.insert(33);
+//        list.insert(23);
+//        list.insert(13);
+//        list.insert(76);
+//        list.display();
+//        list.delete(23);
+//        list.display();
+
+
